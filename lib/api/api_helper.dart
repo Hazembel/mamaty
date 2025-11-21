b0982d235891fb4555b48a5b35af3d4334335094
Hazem Belhadj Salah <hazembelhadjsalah@gmail.com>
2025-11-21 17:03:35 +0100

    apichange

--- lib/api/api_helper.dart
+++ lib/api/api_helper.dart
@@ -4,11 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 //import 'package:flutter/foundation.dart';
 
 class ApiHelper {
-  //static const String baseUrl = "https://mamaty-backend.onrender.com/myapp";
-
-  //local
-  //static const String baseUrl = "http://localhost:3000/myapp";
-static const String baseUrl = 'http://192.168.1.17:3000/myapp';
+  static const String baseUrl = "https://mamaty-backend.onrender.com/myapp";
 
   /// Read token from SharedPreferences
   static Future<String?> getToken() async {
